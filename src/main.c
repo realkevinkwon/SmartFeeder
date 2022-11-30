90fbedf1a1d809fd6fee05996ccced61b04af692
realkevinkwon <kevinkwonmail@gmail.com>
2022-11-30 17:26:03 -0500

    add print statements

--- src/main.c
+++ src/main.c
@@ -15,8 +15,6 @@
 #include "load_cell.h"
 #include "ultrasonic.h"
 
-#define LED_BUILTIN GPIO_NUM_2
-
 #define LOAD_CELL_NAMESPACE "load_cell_data"
 #define ULTRASONIC_NAMESPACE "ultrasonic_data"
 
@@ -24,44 +22,29 @@ void app_main() {
     uint32_t current_millis;
     uint32_t previous_millis = 0;
     uint8_t display_delay = 0;
-    uint16_t led = 0;
-    uint8_t led_state = 0;
     uint32_t micros_start, micros_end;
     gpio_config_t io_cfg = {0};
 
     io_cfg.intr_type = GPIO_PIN_INTR_DISABLE;
     io_cfg.mode = GPIO_MODE_OUTPUT;
-    io_cfg.pin_bit_mask = BIT(LED_BUILTIN);
     gpio_config(&io_cfg);
 
+    printf("Running EVE_init_spi() ... ");
     EVE_init_spi();     // initialize LCD as SPI device
+    printf("Done\n");
+    printf("Running TFT_init() ... ");
     TFT_init();         // run FT813 (LCD graphics chip) initialization sequence
+    printf("Done\n");
+    printf("Running mem_init() ... ");
     mem_init();         // initialize read/write for internal memory
+    printf("Done\n");
 
-    while(1)
-    {
-
+    while (1) {
        	current_millis = (uint32_t) (esp_timer_get_time() / 1000ULL);
 
         if((current_millis - previous_millis) > 4) /* execute the code every 5 milli-seconds */
         {
             previous_millis = current_millis;
-
-            led++;
-            if(led > 69)
-            {
-                led = 0;
-                if(led_state)
-                {
-                    led_state = 0;
-                    gpio_set_level(LED_BUILTIN, 0);
-                }
-                else
-                {
-                    led_state = 42;
-                    gpio_set_level(LED_BUILTIN, 1);
-                }
-            }
 
             micros_start = (uint32_t) (esp_timer_get_time());
             TFT_touch();
