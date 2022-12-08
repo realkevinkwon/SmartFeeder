9c68f4e94b7c12ff76eab2ec4c0cb5ad3f34082c
realkevinkwon <kevinkwonmail@gmail.com>
2022-12-08 14:17:51 -0500

    begin integrating files

--- include/loadcell.h
+++ include/loadcell.h
@@ -1,11 +1,34 @@
-#ifndef __LOAD_CELL_H__
-#define __LOAD_CELL_H__
+#ifndef __LOADCELL_H__
+#define __LOADCELL_H__
 
+#include <stdio.h>
+#include <inttypes.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/gpio.h"
 #include "hx711.h"
 
-void load_cell_init(void);
-void load_cell_write(void);
-void load_cell_read(void);
+#define LC_NUM_MEASUREMENTS 8
 
-#endif /* __LOAD_CELL_H__ */
+#define LC1_DOUT GPIO_NUM_18
+#define LC1_PD_SCK GPIO_NUM_19
+#define LC1_init() load_cell_init(LC1_DOUT, LC1_PD_SCK)
+
+#define LC1_CALI_ZERO 858747637
+#define LC1_CALI_ONE 241.0
+#define LC1_to_grams(x) (x - LC1_CALI_ZERO) / LC1_CALI_ONE;
+
+#define LC2_DOUT GPIO_NUM_5
+#define LC2_PD_SCK GPIO_NUM_19
+#define LC2_init() load_cell_init(LC2_DOUT, LC2_PD_SCK)
+
+#define LC2_CALI_ZERO 309132
+#define LC2_CALI_ONE 234.0
+#define LC2_to_grams(x) (x - LC2_CALI_ZERO) / LC2_CALI_ONE;
+
+hx711_t load_cell_init(gpio_num_t dout, gpio_num_t pd_sck);
+int32_t load_measure(hx711_t load_cell);
+void load_cell_test (void *pvParameters);
+
+#endif /* __LOADCELL_H__ */
--- src/components/loadcell/loadcell.c
+++ src/components/loadcell/loadcell.c
@@ -1,42 +1,52 @@
-#include <esp_log.h>
-#include "driver/gpio.h"
-#include "hx711.h"
-#include "memory.h"
 #include "loadcell.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
-static hx711_t dev = {
-    .dout = GPIO_NUM_18,
-    .pd_sck = GPIO_NUM_19,
-    .gain = HX711_GAIN_A_64
-};
+// Tag for logging
+static const char *TAG = "Load Cell";
 
-static const char* TAG = "HX711";
-
-void load_cell_init(void) {
-    ESP_ERROR_CHECK(hx711_init(&dev));
+hx711_t load_cell_init(gpio_num_t dout, gpio_num_t pd_sck) {
+    hx711_t load_cell = {
+        .dout = dout,
+        .pd_sck = pd_sck,
+        .gain = HX711_GAIN_A_64
+    };
+    ESP_ERROR_CHECK(hx711_init(&load_cell));
+    return load_cell;
 }
 
-void load_cell_write(void) {
-    while (1) {
-        esp_err_t r = hx711_wait(&dev, 500);
-        if (r != ESP_OK) {
-            ESP_LOGE(TAG, "Device not found: %d (%s)\n", r, esp_err_to_name(r));
-            continue;
-        }
+int32_t load_measure(hx711_t load_cell) {
+    esp_err_t r = hx711_wait(&load_cell, 500);
+    if (r != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Device not found: %d (%s)\n", r, esp_err_to_name(r));
+        return -1;
+    }
 
-        int32_t data;
-        r = hx711_read_average(&dev, 5, &data);
-        if (r != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to read data: %d (%s)\n", r, esp_err_to_name(r));
-            continue;
-        }
+    int32_t data;
+    r = hx711_read_average(&load_cell, LC_NUM_MEASUREMENTS, &data);
+    if (r != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to read Data: %d (%s)\n", r, esp_err_to_name(r));
+        return -1;
+    }
 
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+    return data;
+}
+
+void load_cell_test (void *pvParameters)
+{
+    hx711_t load_cell_1 = LC1_init();
+    hx711_t load_cell_2 = LC2_init();
+
+    while (1)
+    {
+        float lc1_meas = LC1_to_grams(load_measure(load_cell_1));
+        float lc2_meas = LC2_to_grams(load_measure(load_cell_2));
+        ESP_LOGI(TAG, "LC1: %fg   -   LC2: %fg", lc1_meas, lc2_meas);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-void load_cell_read(void) {
-
+void app_main()
+{
+    xTaskCreate(load_cell_test, "load_cell_test", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
 }
